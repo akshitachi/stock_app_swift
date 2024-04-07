683488646cb88834a45588236ff683d4338f22b9
Akshil <52787095+akshitachi@users.noreply.github.com>
2024-04-07 12:16:49 -0700

    twittershare

--- Stock App CSCI 571/NewsView.swift
+++ Stock App CSCI 571/NewsView.swift
@@ -3,29 +3,43 @@ import Kingfisher
 
 struct NewsArticleView: View {
     let newsItem: NewsItem
-
-    var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-            KFImage(URL(string: newsItem.image))
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: UIScreen.main.bounds.width - 40, height: 200)
-                .cornerRadius(10)
-            HStack{
-                Text(newsItem.source)
-                    .font(.subheadline)
-                    .bold()
-                    .foregroundColor(.secondary)
-                Text(relativeTime(from: newsItem.datetime))
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
-            }
-            Text(newsItem.headline)
-                .font(.headline)
-                .fontWeight(.bold)
-            Divider()
+    @State private var showingSheet = false
+    var formattedDate: String {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MMMM dd, yyyy"
+        return dateFormatter.string(from: Date(timeIntervalSince1970: TimeInterval(newsItem.datetime)))
         }
-        .padding(.horizontal)
+    var body: some View {
+        Button(action: {
+            showingSheet.toggle()
+        }) {
+            VStack(alignment: .leading, spacing: 10) {
+                KFImage(URL(string: newsItem.image))
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: UIScreen.main.bounds.width - 40, height: 200)
+                    .cornerRadius(10)
+                HStack{
+                    Text(newsItem.source)
+                        .font(.subheadline)
+                        .bold()
+                        .foregroundColor(.secondary)
+                    Text(relativeTime(from: newsItem.datetime))
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
+                Text(newsItem.headline)
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.primary)
+                    .multilineTextAlignment(.leading)
+                Divider()
+            }
+            .padding(.horizontal)
+        }
+        .sheet(isPresented: $showingSheet) {
+            SheetView(source: newsItem.source,headline: newsItem.headline,datetime: formattedDate,summary: newsItem.summary,url:newsItem.url)
+                }
     }
     private func relativeTime(from timestamp: Int) -> String {
         let currentTime = Int(Date().timeIntervalSince1970)
@@ -50,40 +64,60 @@ struct NewsArticleView: View {
 
 struct NewsView: View {
     let news: [NewsItem]
-
+    @State private var showingSheetDict: [Int: Bool] = [:]
+   
     var body: some View {
         VStack(alignment: .leading) {
             Text("News")
                 .font(.title)
-                .padding(.bottom, 10)
+                .padding()
             if let firstArticle = news.first {
                 NewsArticleView(newsItem: firstArticle)
             }
             ForEach(news.dropFirst(), id: \.id) { newsItem in
-                HStack{
-                    VStack(alignment: .leading, spacing: 10) {
-                        HStack{
-                            Text(newsItem.source)
-                                .font(.subheadline)
-                                .bold()
-                                .foregroundColor(.secondary)
-                            Text(relativeTime(from: newsItem.datetime))
-                                .font(.subheadline)
-                                .foregroundColor(.secondary)
-                        }
-                      
-                        Text(newsItem.headline)
-                            .font(.headline)
-                            .fontWeight(.bold)
-                        Divider()
+                let isSheetPresented = Binding<Bool>(
+                        get: { showingSheetDict[newsItem.id] ?? false },
+                        set: { showingSheetDict[newsItem.id] = $0 }
+                    )
+                var formattedDate: String {
+                        let dateFormatter = DateFormatter()
+                        dateFormatter.dateFormat = "MMMM dd, yyyy"
+                    return dateFormatter.string(from: Date(timeIntervalSince1970: TimeInterval(newsItem.datetime)))
                     }
-                    .padding(.horizontal)
-                    KFImage(URL(string: newsItem.image))
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: UIScreen.main.bounds.width - 280, height: 100)
-                        .cornerRadius(10)
-                }
+                Button(action: {
+                    showingSheetDict[newsItem.id, default: false].toggle()
+                }) {
+                    HStack{
+                        VStack(alignment: .leading, spacing: 10) {
+                            HStack{
+                                Text(newsItem.source)
+                                    .font(.subheadline)
+                                    .bold()
+                                    .foregroundColor(.secondary)
+                                Text(relativeTime(from: newsItem.datetime))
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                            }
+                            
+                            Text(newsItem.headline)
+                                .font(.headline)
+                                .fontWeight(.bold)
+                                .foregroundColor(.primary)
+                                .multilineTextAlignment(.leading)
+                            Divider()
+                        }
+                        .padding(.horizontal)
+                        KFImage(URL(string: newsItem.image))
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: UIScreen.main.bounds.width - 280, height: 100)
+                            .cornerRadius(10)
+                    }
+                    .sheet(isPresented: isSheetPresented)
+                    {
+                        SheetView(source: newsItem.source,headline: newsItem.headline,datetime: formattedDate,summary: newsItem.summary,url:newsItem.url)
+                    }
+            }
             }
         }
         .padding()
---
+++ Stock App CSCI 571/SheetView.swift
@@ -0,0 +1,84 @@
+//
+//  SheetView.swift
+//  Stock App CSCI 571
+//
+//  Created by Akshil Shah on 4/6/24.
+//
+
+import SwiftUI
+
+struct SheetView: View {
+    @Environment(\.dismiss) var dismiss
+    @State var source:String
+    @State var headline:String
+    @State var datetime: String
+    @State var summary:String
+    @State var url:String
+//    @State var twitterShare:String = "https://twitter.com/intent/tweet?text=\(headline)&url=\(url)"
+    var body: some View {
+        NavigationView {
+            VStack(alignment:.leading) {
+                Text(source)
+                    .font(.title)
+                    .bold()
+                Text(datetime)
+                    .foregroundColor(.secondary)
+                    .padding(.bottom,10)
+                Divider()
+                Text(headline)
+                    .bold()
+                    .font(.title3)
+                    .multilineTextAlignment(.leading)
+                Text(summary)
+                    .multilineTextAlignment(.leading)
+                    .font(.callout)
+                HStack{
+                    Text("For more details click")
+                        .foregroundColor(.secondary)
+                        .font(.callout)
+                    Link(destination: URL(string: url)!) {
+                        Text("here")
+                            .foregroundColor(.blue)
+                            .font(.callout)
+                    }
+                }
+                HStack{
+                    Link(destination: URL(string: "https://twitter.com/intent/tweet?text=\(headline)&url=\(url)")!) {
+                            Image("twitter")
+                                .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 50, height: 50)
+                                    .clipped()
+                        }
+                    Button(action: {
+                            // Action to perform when Facebook icon is tapped
+                        }) {
+                            Image("facebook")
+                                .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 50, height: 50)
+                                    .clipped()
+                        }
+                }
+                Spacer()
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .navigationBarItems(trailing:
+                Button(action: {
+                    dismiss()
+                }) {
+                    Image(systemName: "xmark")
+                        .font(.callout)
+                        .foregroundColor(.black)
+                }
+            )
+        }
+        .accentColor(.black) 
+    }
+}
+struct SheetView_Previews : PreviewProvider {
+    static var previews : some View {
+       StockData(displaySymbol: "AAPL")
+    }
+}
